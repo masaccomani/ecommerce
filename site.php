5bdc997e4216953f89480f2f0e200f23dabb6974
masaccomani <maxandrey@gmail.com>
2023-12-04 18:37:01 -0300

    master

--- site.php
+++ site.php
@@ -1,5 +1,6 @@
 <?php
 
+use Hcode\Model\Category;
 use Hcode\Model\Product;
 use \Hcode\Page;
 
@@ -13,6 +14,18 @@ $app->get('/', function() {
         'products'=>Product::checkList($products)
     ]);
 
+});
+
+$app->get("/categories/:idcategory", function($idcategory){
+
+    $category = new Category();
+    $category->get((int)$idcategory);
+    $page = new Page();
+
+    $page->setTpl("category", [
+        'category'=>$category->getValues(),
+        'products'=>[]
+    ]);
 });
 
 ?>
