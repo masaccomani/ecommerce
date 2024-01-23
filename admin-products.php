a9d760c5a31750951df437e06b920fe059227e4f
masaccomani <maxandrey@gmail.com>
2024-01-23 18:53:32 -0300

    Carrinho calculo de frete

--- admin-products.php
+++ admin-products.php
@@ -58,16 +58,13 @@ $app->get("/admin/products/:idproduct", function($idproduct){
 $app->post("/admin/products/:idproduct", function($idproduct){
 
     User::verifyLogin();
-
     $product = new Product();
-
     $product->get((int)$idproduct);
-
     $product->setData($_POST);
-
     $product->save();
 
-    $product->setPhoto($_FILES["file"]);
+    if($_FILES["file"]["name"] !=="")
+        $product->setPhoto($_FILES["file"]);
 
     header('Location: /admin/products');
     exit;
--- site.php
+++ site.php
@@ -62,7 +62,8 @@ $app->get("/cart", function(){
     $page = new Page();
     $page->setTpl("cart",[
         'cart'=>$cart->getValues(),
-        'products'=>$cart->getProducts()
+        'products'=>$cart->getProducts(),
+        'error'=>Cart::getMsgError()
     ]);
 });
 
@@ -103,4 +104,14 @@ $app->get("/cart/:idproduct/remove", function($idproduct){
     header("Location: /cart");
     exit;
 });
+
+$app->post("/cart/freight", function(){
+
+    $cart = Cart::getFromSession();
+    $cart->setFreight($_POST['zipcode']);
+
+    header("Location: /cart");
+    exit;
+});
+
 ?>
